2605f683451ecd5f1cbbd4782aa11a79c1269d2a
Ashish Venkat <venkat@virginia.edu>
2021-08-08 18:36:58 -0400

    update microbenchmark
    Change-Id: I82a293fea7fc10f5fc507f1433b2db3ac24c23d9

--- microbench.cpp
+++ microbench.cpp
@@ -3,19 +3,31 @@
 
 uint64_t a[100], b[100], c[100];
 
-void sum() {
-    for (int i = 0; i < 100; i++) {
+void sum1() {
+    for (int i = 0; i < 100; i+=2) {
+        c[i] = a[i] | b[i];
+    }
+}
+void sum2() {
+    for (int i = 1; i < 100; i+=2) {
         c[i] = a[i] | b[i];
     }
 }
 
 int main() { 
-    for (int i = 0; i < 100; i++) {
-        a[i] = rand() % 10000;
-        b[i] = rand() % 1000;
+    for (int i = 0; i < 100; i+=2) {
+        a[i] = 3;
+        b[i] = 4;
+    }
+    for (int i = 1; i < 100; i+=2) {
+        a[i] = 4;
+        b[i] = 5;
     }
     for (int i = 0; i < 5000; i++) {
-        sum();
+        sum1();
+    }
+    for (int i = 0; i < 2000; i++) {
+        sum2();
     }
     for (int i = 0; i < 100; i++) {
         std::cout << c[i] << "\n";
