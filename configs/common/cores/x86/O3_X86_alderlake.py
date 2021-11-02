a62b82c651509983f977e15254db0816a271ed6a
Ashish Venkat <venkat@virginia.edu>
2021-11-02 17:59:10 -0400

    add alder lake
    Change-Id: I97b2b5c0a32c4972dcbdb92288b7244add458f78

--- configs/common/cores/x86/O3_X86_alderlake.py
+++ configs/common/cores/x86/O3_X86_alderlake.py
@@ -4,9 +4,9 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met: redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer;
+# notalder, this list of conditions and the following disclaimer;
 # redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
+# notalder, this list of conditions and the following disclaimer in the
 # documentation and/or other materials provided with the distribution;
 # neither the name of the copyright holders nor the names of its
 # contributors may be used to endorse or promote products derived from
@@ -30,19 +30,19 @@
 from m5.objects import *
 
 # Simple ALU Instructions have a latency of 1
-class O3_X86_icelake_Simple_Int(FUDesc):
+class O3_X86_alderlake_Simple_Int(FUDesc):
     opList = [ OpDesc(opClass='IntAlu', opLat=1) ]
-    count = 4
+    count = 5
 
 # Complex ALU instructions have a variable latencies
-class O3_X86_icelake_Complex_Int(FUDesc):
+class O3_X86_alderlake_Complex_Int(FUDesc):
     opList = [ OpDesc(opClass='IntMult', opLat=4, pipelined=True),
                OpDesc(opClass='IntDiv', opLat=5, pipelined=False) ]
     count = 2
 
 
 # Floating point and SIMD instructions
-class O3_X86_icelake_FP(FUDesc):
+class O3_X86_alderlake_FP(FUDesc):
     opList = [ OpDesc(opClass='SimdAdd', opLat=1),
                OpDesc(opClass='SimdAddAcc', opLat=1),
                OpDesc(opClass='SimdAlu', opLat=1),
@@ -75,31 +75,31 @@ class O3_X86_icelake_FP(FUDesc):
 
 
 # Load/Store Units
-class O3_X86_icelake_Load(FUDesc):
+class O3_X86_alderlake_Load(FUDesc):
     opList = [ OpDesc(opClass='MemRead',opLat=4),
                OpDesc(opClass='FloatMemRead',opLat=4) ]
-    count = 2
+    count = 3
 
-class O3_X86_icelake_Store(FUDesc):
+class O3_X86_alderlake_Store(FUDesc):
     opList = [ OpDesc(opClass='MemWrite',opLat=2),
                OpDesc(opClass='FloatMemWrite',opLat=2) ]
     count = 4
 
 # Functional Units for this CPU
-class O3_X86_icelake_FUP(FUPool):
-    FUList = [O3_X86_icelake_Simple_Int(), O3_X86_icelake_Complex_Int(),
-              O3_X86_icelake_Load(), O3_X86_icelake_Store(), O3_X86_icelake_FP()]
+class O3_X86_alderlake_FUP(FUPool):
+    FUList = [O3_X86_alderlake_Simple_Int(), O3_X86_alderlake_Complex_Int(),
+              O3_X86_alderlake_Load(), O3_X86_alderlake_Store(), O3_X86_alderlake_FP()]
 
 # Bi-Mode Branch Predictor
-class O3_X86_icelake_BP(LTAGE):
-    BTBEntries = 8192
+class O3_X86_alderlake_BP(LTAGE):
+    BTBEntries = 12288
     BTBTagSize = 18
     RASSize = 64
     instShiftAmt = 2
 
-class O3_X86_icelake_1(DerivO3CPU):
-    LQEntries = 128
-    SQEntries = 72
+class O3_X86_alderlake_1(DerivO3CPU):
+    LQEntries = 192
+    SQEntries = 128
     LSQDepCheckShift = 0
     LFSTSize = 1024
     SSITSize = 1024
@@ -113,62 +113,62 @@ class O3_X86_icelake_1(DerivO3CPU):
     iewToRenameDelay = 1
     commitToRenameDelay = 1
     commitToIEWDelay = 1
-    fetchWidth = 6
+    fetchWidth = 8
     fetchBufferSize = 32
     fetchToDecodeDelay = 3
-    decodeWidth = 6
+    decodeWidth = 8
     decodeToRenameDelay = 2
-    renameWidth = 6
+    renameWidth = 8
     renameToIEWDelay = 1
     issueToExecuteDelay = 1
-    dispatchWidth = 10
-    issueWidth = 10
-    wbWidth = 10
-    fuPool = O3_X86_icelake_FUP()
+    dispatchWidth = 12
+    issueWidth = 12
+    wbWidth = 12
+    fuPool = O3_X86_alderlake_FUP()
     iewToCommitDelay = 1
     renameToROBDelay = 1
-    commitWidth = 10
-    squashWidth = 10
+    commitWidth = 12
+    squashWidth = 12
     trapLatency = 13
-    backComSize = 10
-    forwardComSize = 10
+    backComSize = 12
+    forwardComSize = 12
     numPhysIntRegs = 988
-    numPhysFloatRegs = 288
-    numPhysVecRegs = 288
-    numIQEntries = 160
-    numROBEntries = 352
+    numPhysFloatRegs = 488
+    numPhysVecRegs = 488
+    numIQEntries = 224
+    numROBEntries = 512
 
     switched_out = False
-    branchPred = O3_X86_icelake_BP()
+    branchPred = O3_X86_alderlake_BP()
 
 # Instruction Cache
-class O3_X86_icelake_ICache(Cache):
+class O3_X86_alderlake_ICache(Cache):
     response_latency = 1
     tag_latency = 1
     data_latency = 1
     response_latency = 1
     mshrs = 2
     tgts_per_mshr = 8
-    size = '32kB'
+    size = '64kB'
     assoc = 8
     is_read_only = True
     writeback_clean = True
 
 # Data Cache
-class O3_X86_icelake_DCache(Cache):
+class O3_X86_alderlake_DCache(Cache):
     tag_latency = 1
     data_latency = 1
     response_latency = 1
     mshrs = 6
     tgts_per_mshr = 8
-    size = '48kB'
+    size = '64kB'
     assoc = 12
-    write_buffers = 12
+    write_buffers = 16
     writeback_clean = True
 
 # TLB Cache
 # Use a cache as a L2 TLB
-class O3_X86_icelakeWalkCache(Cache):
+class O3_X86_alderlakeWalkCache(Cache):
     data_latency = 2
     tag_latency = 2
     response_latency = 1
@@ -183,31 +183,31 @@ class O3_X86_icelakeWalkCache(Cache):
 
 
 # L2 Cache
-class O3_X86_icelakeL2(Cache):
+class O3_X86_alderlakeL2(Cache):
     tag_latency = 8
     data_latency = 8
     response_latency = 8
     mshrs = 16
     tgts_per_mshr = 8
-    size = '512kB'
-    assoc = 8
+    size = '1280kB'
+    assoc = 20
     write_buffers = 16
     prefetch_on_access = True
     clusivity = 'mostly_excl'
     # Simple stride prefetcher
-    prefetcher = StridePrefetcher(degree=8, latency = 1)
+    prefetcher = StridePrefetcher(degree=16, latency = 1)
     tags = BaseSetAssoc()
     repl_policy = RandomRP()
 
     # L3 Cache
-class O3_X86_icelakeL3(Cache):
+class O3_X86_alderlakeL3(Cache):
     tag_latency = 29
     data_latency = 29
     response_latency = 29
     mshrs = 512
     tgts_per_mshr = 20
-    size = '2MB'
-    assoc = 16
+    size = '3MB'
+    assoc = 12
     write_buffers = 256
     prefetch_on_access = True
     clusivity = 'mostly_excl'
