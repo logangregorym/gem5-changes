fb1cc3998749e82853fce2080063aedbfcdd8c07
logan <lgm4xn@virginia.edu>
2022-08-24 16:28:46 -0400

    fix memory corruption due to uninitialized value
    Change-Id: Ia04a6d6de65491eae1e7eaff3008ba5e701a1472

--- src/arch/x86/decoder.hh
+++ src/arch/x86/decoder.hh
@@ -80,8 +80,8 @@ class Decoder
     static ByteTable ImmediateTypeThreeByte0F38;
     static ByteTable ImmediateTypeThreeByte0F3A;
     static ByteTable ImmediateTypeVex[10];
-    bool uopCacheActive;
-    bool speculativeCacheActive;
+    bool uopCacheActive = 0;
+    bool speculativeCacheActive = 0;
 
   protected:
     struct InstBytes
